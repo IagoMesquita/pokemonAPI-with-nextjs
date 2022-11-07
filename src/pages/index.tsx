038c6f8b65c685da1c898655f5373a019c66a9b2
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-11-07 19:22:42 -0300

    refactor: add span in Image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,12 +16,14 @@ const Home: NextPage<PokemonsProps> = ({ pokemons }: PokemonsProps) => {
         <h1 className={styles.title}>
           Poke<span>Next</span>
         </h1>
-        <Image
-          src="/images/pokeball.png"
-          width="50"
-          height="50"
-          alt="pokeball"
-        />
+        <span>
+          <Image
+            src="/images/pokeball.png"
+            width="50"
+            height="50"
+            alt="pokeball"
+          />
+        </span>
       </div>
       <div className={styles.pokemons_container}>
         {pokemons.map((pokemon) => (
@@ -35,21 +37,20 @@ const Home: NextPage<PokemonsProps> = ({ pokemons }: PokemonsProps) => {
 export async function getStaticProps() {
   const maxPokemons = 251;
   const api = "https://pokeapi.co/api/v2/pokemon/";
-try {
-  const res = await fetch(`${api}?limit=${maxPokemons}`);
-  const data = await res.json();
+  try {
+    const res = await fetch(`${api}?limit=${maxPokemons}`);
+    const data = await res.json();
 
-  // Add id
-  const pokemons: IResults[] = data.results;
-  pokemons.forEach((item, index) => (item.id = index + 1));
+    // Add id
+    const pokemons: IResults[] = data.results;
+    pokemons.forEach((item, index) => (item.id = index + 1));
 
-  return {
-    props: { pokemons },
-  };
-} catch (error: any) {
-  console.error('Request API', error.message);
-}
-  
+    return {
+      props: { pokemons },
+    };
+  } catch (error: any) {
+    console.error("Request API", error.message);
+  }
 }
 
 export default Home;
